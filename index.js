491836e959366810e2bcbd2f8a47655307cc6d1c
MetaSkills178 <153375101+MetaSkills178@users.noreply.github.com>
2024-03-12 17:30:39 +0000

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Recrutement staff","Playing MetaStudio games"];
 
 
 let currentIndex = 0;
